72d2b64658ff0c1004124c3af44825e7f2545c54
lndlazy <lndlazy@163.com>
2023-02-16 11:59:34 +0800

    home first commit

---
+++ module_home/src/main/java/com/lee/module_home/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.lee.module_home
+
+import com.lee.commontest.base.BaseActivity
+
+class HomeActivity : BaseActivity() {
+    override fun getViewId(): Int = R.layout.activity_home
+
+
+    override fun initData() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+
+}
