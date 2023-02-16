9f1c3c7a406f07ce9bc51dce5396d86261dd8651
lndlazy <lndlazy@163.com>
2023-02-16 13:57:46 +0800

    添加路由跳转及参数

--- module_home/src/main/java/com/lee/module_home/HomeActivity.kt
+++ module_home/src/main/java/com/lee/module_home/HomeActivity.kt
@@ -1,17 +1,32 @@
 package com.lee.module_home
 
+import android.widget.Button
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.launcher.ARouter
 import com.lee.commontest.base.BaseActivity
 
+@Route(path = "/home/homeActivity")
 class HomeActivity : BaseActivity() {
+
     override fun getViewId(): Int = R.layout.activity_home
 
-
     override fun initData() {
+        val stringExtra = intent?.getStringExtra("str")
+        val intExtra = intent?.getIntExtra("int", -1)
+        val booleanExtra = intent?.getBooleanExtra("boolean", false)
+        println("HomeActivity ==>>> str:$stringExtra, intExtra:${intExtra}, booleanExtra:$booleanExtra")
 
     }
 
     override fun initView() {
-
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            //cart跳转到首页
+            ARouter.getInstance().build("cart/cartActivity")
+                .withString("str", "homeActivity跳转过来的")
+                .withInt("int", 666)
+                .withBoolean("boolean", true)
+                .navigation()
+        }
     }
 
 
